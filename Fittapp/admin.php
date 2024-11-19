76ad778260ae33bf7f852600e8180d8cb8b109ab
gylm99 <112557672+gylm99@users.noreply.github.com>
2024-11-19 20:34:28 +0100

    Update admin.php

--- Fittapp/admin.php
+++ Fittapp/admin.php
@@ -18,7 +18,7 @@ $username = "root";  // a saját felhasználóneved
 $password = "";      // a saját jelszavad
 $dbname = "fitnessdb";
 
-
+//ab kapcsolat
 try {
     $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -26,22 +26,56 @@ try {
     die("Adatbázis kapcsolódási hiba: " . $e->getMessage());
 }
 
-
+// edzés felvitele
 if($_SERVER['REQUEST_METHOD']=='POST' && isset($_POST['submTraining']) && !empty($pdo)){
     $traningName=$_POST['traningName'];
     $traningCaloriesBurn=$_POST['traningCaloriesburn'];  
     try{
-        $sqlFood="INSERT INTO training (trainingname,caloriesburn) VALUES(:traningName,:traningCaloriesBurn)";
+        $sqlTraning="INSERT INTO training (trainingname,caloriesburn) VALUES(:traningName,:traningCaloriesBurn)";
         $traningCaloriesBurn=floatval($traningCaloriesBurn);
-        $querryFood=$pdo->prepare($sqlFood);
-        $querryFood->bindParam(":traningName",$traningName,PDO::PARAM_STR);
-        $querryFood->bindParam(':traningCaloriesBurn',$traningCaloriesBurn, PDO::PARAM_STR);
-        $querryFood->execute();
+        $querryTraning=$pdo->prepare($sqlTraning);
+        $querryTraning->bindParam(":traningName",$traningName,PDO::PARAM_STR);
+        $querryTraning->bindParam(':traningCaloriesBurn',$traningCaloriesBurn, PDO::PARAM_STR);
+        $querryTraning->execute();
 
 
     }catch(PDOException $ex){
         $error="Hiba történt".$ex->getMessage();
+        die();
     }
+}
+
+//étel velvitel
+if($_SERVER['REQUEST_METHOD']=="POST" && isset($_POST['submFood']) && !empty($pdo)){
+    $foodName=$_POST['foodName'];
+    $foodCalories=$_POST['foodCalories'];
+    $foodCarb=$_POST['foodCarb'];
+    $foodProtein=$_POST['foodProtein'];
+    $foodFat=$_POST['foodFat'];
+    try{
+        $foodCalories=floatval($foodCalories);
+        $foodCarb=floatval($foodCarb);
+        $foodProtein=floatval($foodProtein);
+        $foodFat=floatval($foodFat);
+
+        echo "<p>$foodCalories</p>";
+        echo "<p>$foodCarb</p>";
+        $sqlFood='INSERT INTO foodtbl (mealname, calories, carb, protein, fat) VALUES(:mealname,:calories,:carb,:protein,:fat)';
+        $queryFood=$pdo->prepare($sqlFood);
+        $queryFood->bindValue(':mealname',$foodName,PDO::PARAM_STR);
+        $queryFood->bindValue(':calories',$foodCalories,PDO::PARAM_STR);
+        $queryFood->bindValue(':carb',$foodCarb,PDO::PARAM_STR);
+        $queryFood->bindValue(':protein',$foodProtein,PDO::PARAM_STR);
+        $queryFood->bindValue(':fat',$foodFat,PDO::PARAM_STR);
+        $queryFood->execute();
+
+
+
+    }catch(PDOException $ex){
+        $error="Adatbéisu hiba történt:".$ex->getMessage();
+        //die();
+    }
+
 }
 
 ?>
@@ -58,12 +92,7 @@ if($_SERVER['REQUEST_METHOD']=='POST' && isset($_POST['submTraining']) && !empty
 </head>
 <body>
 
-<?php 
-if(!empty($error)){
-    echo "<p class='error'>$error</p>";
-}
 
-?>
 
 <h1>Admin site</h1>
 
@@ -71,8 +100,8 @@ if(!empty($error)){
 <h1>Edzés felvitele:</h1>
 
 <form action="<?= htmlspecialchars($_SERVER['PHP_SELF'])?>" method="post">
-    <fieldset>
-    <label id="traningName">Étel neve</label>
+    <fieldset> Edzés felvitel
+    <label id="traningName">Edzés neve</label>
         <input type="text" name="traningName" id="traningName">
     <label id="traningCaloriesburn">Kalória értéke</label>
         <input type="number" step="0.01" name="traningCaloriesburn" id="traningCaloriesburn">
@@ -80,6 +109,32 @@ if(!empty($error)){
     <input type="submit" value="Mentés" name="submTraining">
 </fieldset>
 </form>
+
+
+<form action="<?=htmlspecialchars(trim($_SERVER['PHP_SELF']))?>" method="post">
+    <fieldset> Étel felvitel
+        <label for="foodName">Név</label>
+        <input type="text" name="foodName" id="foodName">
+        <label for="foodCalories">Kalória</label>
+        <input type="number" step="0.01" name="foodCalories" id="foodCalories">
+        <label for="foodCarb">Carb</label>
+        <input type="number" step="0.01" name="foodCarb" id="foodCarb">
+        <label for="foodProtein">Protein</label>
+        <input type="number" step="0.01" name="foodProtein" id="foodProtein">
+        <label for="foodFat">Zsír</label>
+        <input type="number" step="0.01" name="foodFat" id="foodFat">
+
+        <input type="submit" value="Mentés" name="submFood">
+
+    </fieldset>
+</form>
+
+
+<?php
+if(!empty($error)){
+    echo "<p class='error'>$error</p>";
+}
+?>
 
 <p><a href="logout.php?logout">Kijelentkezés</a></p>
     
